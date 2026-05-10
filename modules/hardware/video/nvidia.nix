236bb286064a373c7ce0b9e1e8024c0c4b0cdebe
Fabien MATHE <f.mathe.pro@outlook.fr>
2026-05-10 17:26:22 +0200

    Add wallpapers

--- modules/hardware/video/nvidia.nix
+++ modules/hardware/video/nvidia.nix
@@ -1,11 +1,12 @@
 {
+  host,
   lib,
   pkgs,
   config,
   ...
 }:
 let
-  nvidiaDriverChannel = config.boot.kernelPackages.nvidiaPackages.stable; # stable, latest, beta, etc.
+  inherit (import ../../../hosts/${host}/variables.nix) nvidiaChannel;
 in
 {
   environment.sessionVariables = lib.optionalAttrs config.programs.hyprland.enable {
@@ -37,11 +38,11 @@ in
 
       modesetting.enable = true; # Modesetting is required.
 
-      package = nvidiaDriverChannel;
+      package = config.boot.kernelPackages.nvidiaPackages.${nvidiaChannel};
     };
     graphics = {
       enable = true;
-      # package = nvidiaDriverChannel;
+      # package = config.boot.kernelPackages.nvidiaPackages.${nvidiaChannel};
       enable32Bit = true;
       extraPackages = with pkgs; [
         nvidia-vaapi-driver
@@ -52,11 +53,10 @@ in
   };
   nixpkgs.config = {
     nvidia.acceptLicense = true;
-    cudaSupport = false;
     allowUnfreePredicate =
       pkg:
       builtins.elem (lib.getName pkg) [
-        #"cudatoolkit"
+        "cudatoolkit"
         "nvidia-persistenced"
         "nvidia-settings"
         "nvidia-x11"
