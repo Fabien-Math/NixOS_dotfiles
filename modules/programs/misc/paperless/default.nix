108bc457c47ccbc93e34e5652bace86901ae6825
Fabien <f.mathe.pro@outlook.fr>
2026-07-02 19:14:34 +0200

    Rename to default.nix

---
+++ modules/programs/misc/paperless/default.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+{
+  services.paperless = {
+    enable = true;
+
+    address = "127.0.0.1";
+
+    passwordFile = "/run/secrets/paperless-password";
+
+    mediaDir = "/srv/paperless/media";
+    consumptionDir = "/srv/paperless/consume";
+
+    settings = {
+      PAPERLESS_OCR_LANGUAGE = "eng";
+      PAPERLESS_TIME_ZONE = "Europe/Paris";
+    };
+  };
+}
